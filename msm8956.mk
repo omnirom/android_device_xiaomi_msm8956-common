77d4afcc5b498065044500c810807f0dfba1475a
khanfrd <khanfrd94@gmail.com>
2014-05-24 11:22:01 -0400

    msm8956-common: XiaomiParts: add Advanced device settings  *https://github.com/omnirom/android_device_xiaomi_msm8956-common/tree/android-7.1/XiaomiParts
    Change-Id: Iacbff37e7ed329974f4308c76c6bd784ae27bf32

--- msm8956.mk
+++ msm8956.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
