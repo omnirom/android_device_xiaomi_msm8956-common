4c73fa2f6685aa318b4d834915c41c54d2b22c4f
khanfrd <khanfrd94@gmail.com>
2017-03-30 20:59:25 +0530

    msm8956: Add omni charger images
    Signed-off-by: khanfrd <khanfrd94@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# OMNI Charger Images
+PRODUCT_PACKAGES += \
+    omni_charger_res_images
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
