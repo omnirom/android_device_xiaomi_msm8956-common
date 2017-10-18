c12031a20431198d3b2d5bde00b9fdb31bb4009e
Adarsh-MR <adarshmr1998@gmail.com>
2017-10-18 13:17:26 +0000

    msm8956-common: remove custom hwui props
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -22,6 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
 
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
