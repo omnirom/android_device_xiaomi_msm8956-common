a161c27fb7231003dd801c1a141e9dbb13245117
TheScarastic <warabhishek@gmail.com>
2017-10-23 09:24:58 +0000

    msm8956-common: build healthd
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -190,6 +190,13 @@ PRODUCT_PACKAGES += \
     sap.conf \
     xtwifi.conf
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-service \
+    android.hardware.health@1.0
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
