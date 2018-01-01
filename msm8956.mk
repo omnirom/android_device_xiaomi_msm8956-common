432aad0d2d92407cd6af7f7d521b024d7c78d0ec
Nick <nickgunale@gmail.com>
2018-01-02 00:22:50 +0530

    ril: build librmnetctl This is requried for ril and wifi to work F/libc    : CANNOT LINK EXECUTABLE "/system/bin/netmgrd": library "librmnetctl.so" not found

--- msm8956.mk
+++ msm8956.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# RIL
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
